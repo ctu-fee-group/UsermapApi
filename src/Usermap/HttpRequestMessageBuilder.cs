9a8b8adeaede33b4eef3322c5c071c02abb45a5b
František Boháček <fandabohacek@gmail.com>
2021-09-20 19:22:53 +0200

    feat: add support methods into usermap http client for getting images and head responses

---
+++ src/Usermap/HttpRequestMessageBuilder.cs
@@ -0,0 +1,84 @@
+//
+//   HttpRequestMessageBuilder.cs
+//
+//   Copyright (c) Christofel authors. All rights reserved.
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Web;
+
+namespace Usermap
+{
+    /// <summary>
+    /// Builder for <see cref="HttpRequestMessage"/>.
+    /// </summary>
+    public class HttpRequestMessageBuilder
+    {
+        private readonly NameValueCollection _query;
+        private string _path;
+        private HttpMethod _method;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpRequestMessageBuilder"/> class.
+        /// </summary>
+        /// <param name="method">The http method.</param>
+        /// <param name="path">The base path.</param>
+        public HttpRequestMessageBuilder(HttpMethod method, string path)
+        {
+            _method = method;
+            _path = path;
+            _query = HttpUtility.ParseQueryString(string.Empty);
+        }
+
+        /// <summary>
+        /// Sets the given path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>The builder.</returns>
+        public HttpRequestMessageBuilder WithPath(string path)
+        {
+            _path = path;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the given http method.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <returns>The builder.</returns>
+        public HttpRequestMessageBuilder WithMethod(HttpMethod method)
+        {
+            _method = method;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the given item to the query parameters.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="value">The value of the parameter.</param>
+        /// <returns>The builder.</returns>
+        public HttpRequestMessageBuilder AddQuery(string name, string value)
+        {
+            _query.Add(name, value);
+            return this;
+        }
+
+        /// <summary>
+        /// Builds the request message.
+        /// </summary>
+        /// <returns>The built message.</returns>
+        public HttpRequestMessage Build()
+        {
+            string urlPath = _path;
+            if (_query.Count > 0)
+            {
+                urlPath += "?" + _query;
+            }
+
+            return new HttpRequestMessage(_method, urlPath);
+        }
+    }
+}
--- src/Usermap/UsermapHttpClient.cs
+++ src/Usermap/UsermapHttpClient.cs
@@ -5,10 +5,12 @@
 //  Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Mime;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -63,22 +65,77 @@ namespace Usermap
         /// <exception cref="Exception">If the response could not be retrieved from the server and <see cref="UsermapApiOptions.ThrowOnError"/> is true.</exception>
         /// <exception cref="InvalidOperationException">If the response could not be retrieved from the server and <see cref="UsermapApiOptions.ThrowOnError"/> is true..</exception>
         public async Task<TEntity?> GetAsync<TEntity>
-            (string path, Action<HttpRequestMessage>? configureRequest = null, CancellationToken token = default)
+            (string path, Action<HttpRequestMessageBuilder>? configureRequest = null, CancellationToken token = default)
         {
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Get, path);
-            configureRequest?.Invoke(requestMessage);
+            using var requestMessage = ConfigureRequest(path, HttpMethod.Get, configureRequest);
+            using var response = await ExecuteRequestAsync(requestMessage, token);
 
-            requestMessage.Headers.Authorization = new AuthenticationHeaderValue
-            (
-                "Bearer",
-                _tokenProvider.AccessToken
-            );
+            if (response is null)
+            {
+                return default;
+            }
 
+            return await ParseResponseEntity<TEntity>(response, token);
+        }
+
+        /// <summary>
+        /// Calls HEAD on the specified url. Returns true for successful response, false for unsuccessful.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="configureRequest">The action that will be called to configure the request.</param>
+        /// <param name="token">The cancellation token for the operation.</param>
+        /// <returns>Whether successful response was obtained.</returns>
+        /// <exception cref="Exception">If the response could not be retrieved from the server and <see cref="UsermapApiOptions.ThrowOnError"/> is true.</exception>
+        /// <exception cref="InvalidOperationException">If the response could not be retrieved from the server and <see cref="UsermapApiOptions.ThrowOnError"/> is true..</exception>
+        public async Task<bool> HeadAsync
+            (string path, Action<HttpRequestMessageBuilder>? configureRequest = null, CancellationToken token = default)
+        {
+            using var requestMessage = ConfigureRequest(path, HttpMethod.Head, configureRequest);
+            using var response = await ExecuteRequestAsync(requestMessage, token);
+
+            if (response is null)
+            {
+                return false;
+            }
+
+            return response.IsSuccessStatusCode;
+        }
+
+        /// <summary>
+        /// Calls GET on the specified url, returns an image.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <param name="configureRequest">The action that will be called to configure the request.</param>
+        /// <param name="token">The cancellation token for the operation.</param>
+        /// <returns>The retrieved image or null if it was not found.</returns>
+        /// <exception cref="Exception">If the response could not be retrieved from the server and <see cref="UsermapApiOptions.ThrowOnError"/> is true.</exception>
+        /// <exception cref="InvalidOperationException">If the response could not be retrieved from the server and <see cref="UsermapApiOptions.ThrowOnError"/> is true..</exception>
+        public async Task<Image?> GetImageAsync
+            (string path, Action<HttpRequestMessageBuilder>? configureRequest = null, CancellationToken token = default)
+        {
+            using var requestMessage = ConfigureRequest(path, HttpMethod.Get, configureRequest);
+            using var response = await ExecuteRequestAsync(requestMessage, token);
+
+            if (response is null)
+            {
+                return null;
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Image.FromStream(await response.Content.ReadAsStreamAsync(token));
+            }
+
+            await HandleErrorResponse(response, token);
+            return null;
+        }
+
+        private async Task<HttpResponseMessage?> ExecuteRequestAsync(HttpRequestMessage request, CancellationToken token)
+        {
             try
             {
                 var client = _httpClientFactory.CreateClient("Usermap");
-                using var response = await client.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, token);
-                return await ParseResponse<TEntity>(response, token);
+                return await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, token);
             }
             catch (Exception e)
             {
@@ -92,7 +149,54 @@ namespace Usermap
             }
         }
 
-        private async Task<TEntity?> ParseResponse<TEntity>(HttpResponseMessage response, CancellationToken token)
+        private HttpRequestMessage ConfigureRequest(string path, HttpMethod method, Action<HttpRequestMessageBuilder>? configureAction)
+        {
+            var requestMessageBuilder = new HttpRequestMessageBuilder(method, path);
+            configureAction?.Invoke(requestMessageBuilder);
+            var requestMessage = requestMessageBuilder.Build();
+
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue
+            (
+                "Bearer",
+                _tokenProvider.AccessToken
+            );
+
+            return requestMessage;
+        }
+
+        private async Task HandleErrorResponse(HttpResponseMessage response, CancellationToken token)
+        {
+            try
+            {
+                using var content = await response.Content.ReadAsStreamAsync(token);
+                var reader = new StreamReader(content);
+                var contentString = await reader.ReadToEndAsync();
+
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return;
+                }
+
+                string errorMessage = $"There was an error with calling usermap api: {response.StatusCode} {response.ReasonPhrase} {contentString}";
+                if (_options.ThrowOnError)
+                {
+                    throw new Exception(errorMessage);
+                }
+
+                _logger.LogError(errorMessage);
+            }
+            catch (Exception e)
+            {
+                if (_options.ThrowOnError)
+                {
+                    throw;
+                }
+
+                _logger.LogError(e, $"There was an error while processing the http response {response.StatusCode} {response.ReasonPhrase}");
+            }
+        }
+
+        private async Task<TEntity?> ParseResponseEntity<TEntity>(HttpResponseMessage response, CancellationToken token)
         {
             if (response.IsSuccessStatusCode)
             {
@@ -106,36 +210,8 @@ namespace Usermap
                 return entity;
             }
 
-            try
-            {
-                using var content = await response.Content.ReadAsStreamAsync(token);
-                var reader = new StreamReader(content);
-                var contentString = await reader.ReadToEndAsync();
-
-                if (response.StatusCode == HttpStatusCode.NotFound)
-                {
-                    return default;
-                }
-
-                string errorMessage = $"There was an error with calling usermap api: {response.StatusCode} {response.ReasonPhrase} {contentString}";
-                if (_options.ThrowOnError)
-                {
-                    throw new Exception(errorMessage);
-                }
-                _logger.LogError(errorMessage);
-                return default;
-            }
-            catch (Exception e)
-            {
-                if (_options.ThrowOnError)
-                {
-                    throw;
-                }
-
-                _logger.LogError(e, $"There was an error while processing the http response {response.StatusCode} {response.ReasonPhrase}");
-
-                return default;
-            }
+            await HandleErrorResponse(response, token);
+            return default;
         }
     }
 }
